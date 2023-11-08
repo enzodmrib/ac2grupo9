44a86379e63f918776453846efd7072cb54359a8
enzodmrib-viamaker <enzo.ribeiro@atlangroup.onmicrosoft.com>
2023-11-08 11:00:47 -0300

    feat: changes base route

--- impl/api/src/server.ts
+++ impl/api/src/server.ts
@@ -2,12 +2,36 @@ import express from 'express'
 import routes from './routes'
 import { initDB } from './database/init'
 import cors from "cors"
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'A sample API for learning Swagger',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.ts'],
+};
+
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 
 const app = express()
 
+app.use('/flightapp', routes)
 app.use(express.json())
 app.use(cors())
-app.use(routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 initDB()
 
